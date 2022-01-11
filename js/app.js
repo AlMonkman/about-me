681c5e5831a7bd7c7a872ee8b32cf756f884a66e
Alan Monkman <almonkman27@gmail.com>
2022-01-11 15:07:28 -0800

    Added all my files and added the html context with some css design

---
+++ js/app.js
@@ -0,0 +1,10 @@
+'use strict';
+
+let user = prompt('What is your name?');
+alert('Welcome ${user}');
+
+let answerOne = prompt('Is my name Alan?').toLowerCase();
+
+if(answerOne === 'yes' || === 'y') {
+  console.log('yes');
+}
