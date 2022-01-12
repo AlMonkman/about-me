dc4e4975f69d666ee8bd5d1400a81335e5294e8d
Alan Monkman <almonkman27@gmail.com>
2022-01-12 15:18:22 -0800

    added question six

--- README.md
+++ README.md
@@ -10,3 +10,16 @@ submission PR
 Any Links you used as reference
 Reflections and Comments
 This lab went very well. The if and else statements didnt give me any trouble. Just CSS slowed me down a bit.
+
+
+LAB - 03
+About me
+
+I am building this webpage to let users know more about myself. Ive added 5 questions with either yes or no answers. I have also put the basic HTML and CSS.
+
+Author: Alan Monkman
+Links and Resources
+submission PR
+Any Links you used as reference
+Reflections and Comments
+This lab went very well. The if and else statements didnt give me any trouble. Just CSS slowed me down a bit.
--- js/app.js
+++ js/app.js
@@ -2,7 +2,9 @@
 
 let user = prompt('What is your name?');
 alert('Welcome to my page ' + user +'!');
+alert('Please answer the following questions with either yes or no.');
 
+// question 1
 
 let answerOne = prompt('Is my name Alan?').toLowerCase();
 
@@ -19,6 +21,7 @@ else{
   alert('Please answer with either yes or no :)');
 }
 
+// question 2
 
 let answerTwo = prompt('Am I from Washington?').toLowerCase();
 
@@ -35,6 +38,7 @@ else{
   alert('Please answer with either yes or no :)');
 }
 
+// question 3
 
 let answerThree = prompt('Was my last job at USPS?').toLowerCase();
 
@@ -51,6 +55,7 @@ else{
   alert('Please answer with either yes or no :)');
 }
 
+// question 4
 
 let answerFour = prompt('Have I been to Vietnam?').toLowerCase();
 
@@ -67,18 +72,150 @@ else{
   alert('Please answer with either yes or no :)');
 }
 
+// question 5
 
 let answerFive = prompt('Did I graduate from Inglemoor High School?').toLowerCase();
 
 if(answerFive === 'yes' || answerFive === 'y'){
   // console.log('yes');
-  alert('That is correct ' + user + '! Great job with the quiz.');
+  alert('That is correct ' + user + '!');
 }
 else if(answerFive === 'no' || answerFive === 'n'){
   // console.log('no');
-  alert('Wrong answer ' + user + '. Refresh the page and try the quiz again!');
+  alert('Wrong answer ' + user + '.');
 }
 else{
   // console.log('follow instructions');
   alert('Please answer with either yes or no :)');
 }
+
+
+// question 6
+
+let myNumber = 17;
+let Guesses = 4;
+
+while(Guesses !== 0){
+  let answerSix = prompt('Alright, ' + user + ' I want you to pick a number from 1 through 27.');
+
+  if(answerSix < myNumber){
+  Guesses --;
+ alert('Too low!');
+}
+
+  if(answerSix > myNumber){
+   Guesses --;
+   alert('Too high!');
+ }
+
+  if(answerSix == myNumber){
+   alert('Wow! Good job, that\'s exactly the number I was thinking.');
+   break;
+ }
+
+ if(Guesses == 0){
+  alert('Good try! My number was actually 17');
+ }
+
+}
+// question 7
+
+let myArr = [mango, banana, kiwi, apple, orange, peach, cantaloupe, watermelon, nectarine];
+
+let fruitGuess = 6;
+
+let faveFruits = 'mango, cantaloupe, banana';
+
+while(fruitGuess !== 0){
+  let answerSeven = prompt('Hey ' + user + 'can you guess what some of my favorite fruits are?').toLowerCase();
+
+  if(answerSeven == faveFruits){
+    alert('Yes! ' + answerSeven + '\'s are one of my favorites!');
+    break;
+  }
+
+  if(fruitGuess == 0){
+    alert('Better luck next time! Mangoes, bananas, and cantaloupes are my favorites!');
+    break;
+  }
+
+  else{
+    fruitGuess --;
+    alert('Sorry, that\'s not one of my favorites. Try again!');
+  }
+
+  
+}
+
+
+
+
+
+// Array
+
+// let myArr = [1, 'hello', [2,3], true];
+// console.log(myArr);
+// console.table(myArr);
+
+// console.log(myArr[0]);
+
+// let favBoybands = ['BTS', 'Backstreet boys', 'Nsync'];
+
+// console.table(favBoyBands);
+
+// let arrLength = favBoyBands.length;
+
+// console.log(favBoyBands[favBoyBands.length-1]); accessing the last element
+
+// console.log(arrLength);
+
+// myArr[4] = false; add to array
+ 
+// favBoyBands.push(); will add to end of array
+
+// favBoyBands.pop(); will remove last element from array
+
+
+// Loops
+
+// For loops
+// great for doing something a certain amount of times
+// great for iterating through/ over arrays
+// i++ means let i = i+1
+
+// for(starting val; condition; increment){
+// code block 
+// }
+
+//  let students = ['alan', 'bob', 'brady', 'lauren', 'nick', 'jefferey', 'steve']
+
+// for(let i = 0; i < students.length; i++){
+// console,log(students[i]);
+// if(students[i] === "steve'){
+// console.log('hey steve');
+// }
+
+
+
+// While loops
+
+// while(condition){
+// do something 
+// }
+ 
+// let myNumber = 3;
+
+// let userNumber = prompt('what number am i thinking of?');
+
+// while(userNumber != myNumber){
+// userNumber = prompt('What number am i thinking of?');
+// }
+// 
+//  let clueGuesses = 20;
+// let suspect = 'Nicholas';
+// let foundSuspect = false;
+
+// while(clueGuesses && !foundSuspect){
+// let ourGuess = getRandomStudent();
+// console.log('it was ${ourGuess}, in Remo with the candlestick');
+// }
