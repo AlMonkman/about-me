9bf8e36fb5e017c967df7ff2b9eb5f0ba43b85ec
Alan Monkman <almonkman27@gmail.com>
2022-01-13 11:43:19 -0800

    code review

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 'use strict';
 
 let user = prompt('What is your name?');
-alert('Welcome to my page ' + user +'!');
+alert('Welcome to my page ' + user + '!');
 alert('Please answer the following questions with either yes or no.');
 
 let total = 0
@@ -10,16 +10,16 @@ let total = 0
 
 let answerOne = prompt('Is my name Alan?').toLowerCase();
 
-if(answerOne === 'yes' || answerOne === 'y') {
+if (answerOne === 'yes' || answerOne === 'y') {
   // console.log('yes');
   alert('Yeah! How\'d you know?');
-  total = total+1;
+  total = total + 1;
 }
-else if(answerOne === 'no' || answerOne === 'n'){
+else if (answerOne === 'no' || answerOne === 'n') {
   // console.log('no');
   alert('Sorry but my name is Alan!');
 }
-else{
+else {
   // console.log('follow instructions');
   alert('Please answer with either yes or no :)');
 }
@@ -28,16 +28,16 @@ else{
 
 let answerTwo = prompt('Am I from Washington?').toLowerCase();
 
-if(answerTwo === 'yes' || answerTwo === 'y'){
+if (answerTwo === 'yes' || answerTwo === 'y') {
   // console.log('yes');
   alert('Yeah! I am a true Washingtonian.');
-  total = total+1;
+  total = total + 1;
 }
-else if(answerTwo === 'no' || answerTwo === "n"){
+else if (answerTwo === 'no' || answerTwo === "n") {
   // console.log('no');
   alert('Sorry! Washington is where I call home.');
 }
-else{
+else {
   // console.log('follow instructions');
   alert('Please answer with either yes or no :)');
 }
@@ -46,16 +46,16 @@ else{
 
 let answerThree = prompt('Was my last job at USPS?').toLowerCase();
 
-if(answerThree === 'yes' || answerThree === 'y'){
+if (answerThree === 'yes' || answerThree === 'y') {
   // console.log('yes');
   alert('Thats correct! I just left the post office a couple weeks ago.');
-  total = total+1;
+  total = total + 1;
 }
-else if(answerThree === 'no' || answerThree ==='n'){
+else if (answerThree === 'no' || answerThree === 'n') {
   // console.log('no');
   alert('Actually my last job was with USPS!');
 }
-else{
+else {
   // console.log('follow instructions');
   alert('Please answer with either yes or no :)');
 }
@@ -64,16 +64,16 @@ else{
 
 let answerFour = prompt('Have I been to Vietnam?').toLowerCase();
 
-if(answerFour === 'yes' || answerFour === 'y'){
+if (answerFour === 'yes' || answerFour === 'y') {
   // console.log('yes');
   alert('Yeah! Vietnam was a very beautiful place to visit.');
-  total = total+1;
+  total = total + 1;
 }
-else if(answerFour === 'no' || answerFour === 'n'){
+else if (answerFour === 'no' || answerFour === 'n') {
   // console.log('no');
   alert('Sorry but I actually spent a month in Vietnam!');
 }
-else{
+else {
   // console.log('follow instructions');
   alert('Please answer with either yes or no :)');
 }
@@ -82,16 +82,16 @@ else{
 
 let answerFive = prompt('Did I graduate from Inglemoor High School?').toLowerCase();
 
-if(answerFive === 'yes' || answerFive === 'y'){
+if (answerFive === 'yes' || answerFive === 'y') {
   // console.log('yes');
   alert('That is correct ' + user + '!');
-  total = total+1;
+  total = total + 1;
 }
-else if(answerFive === 'no' || answerFive === 'n'){
+else if (answerFive === 'no' || answerFive === 'n') {
   // console.log('no');
   alert('Wrong answer ' + user + '.');
 }
-else{
+else {
   // console.log('follow instructions');
   alert('Please answer with either yes or no :)');
 }
@@ -102,44 +102,45 @@ else{
 let myNumber = 17;
 let Guesses = 4;
 
-while(Guesses !== 0){
+while (Guesses !== 0) {
   let answerSix = prompt('Alright, ' + user + ' I want you to pick a number from 1 through 27.');
 
-  if(answerSix < myNumber){
-  Guesses --;
- alert('Too low!');
+  if (answerSix < myNumber) {
+    Guesses--;
+    alert('Too low!');
+  }
+
+  if (answerSix > myNumber) {
+    Guesses--;
+    alert('Too high!');
+  }
+
+  if (+answerSix === myNumber) {
+    alert('Wow! Good job, that\'s exactly the number I was thinking.');
+    total = total + 1;
+    break;
+  }
+
+  if (Guesses === 0) {
+    alert('Good try! My number was actually 17');
+  }
+
 }
 
-  if(answerSix > myNumber){
-   Guesses --;
-   alert('Too high!');
- }
-
-  if(answerSix == myNumber){
-   alert('Wow! Good job, that\'s exactly the number I was thinking.');
-   total = total+1;
-   break;
- }
-
- if(Guesses == 0){
-  alert('Good try! My number was actually 17');
- }
-
-}
 // question 7
 
 let myArr = ['mango', 'banana', 'cantaloupe'];
 
 let fruitGuess = 6;
 
-while(fruitGuess !== 0){
+while (fruitGuess !== 0) {
   let answerSeven = prompt('Hey ' + user + ' can you guess what some of my favorite fruits are?').toLowerCase();
 
-  for(let i = 0; i < myArr.length; i++){
+  for (let i = 0; i < myArr.length; i++) {
 
-    if(answerSeven === myArr[i]){
+    if (answerSeven === myArr[i]) {
       alert('Yes! ' + answerSeven + '\'s are one of my favorites!');
-      total = total+1;
+      total = total + 1;
       fruitGuess = 1;
       break;
     }
